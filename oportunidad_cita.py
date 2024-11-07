0237b6341c01afea1e62c7f45b2c398a9f73781d
Daniel Alfaro Rojas <alfaro_daniel@hotmail.com>
2024-11-07 10:24:15 -0500

    Actualizando documentación

--- oportunidad_cita.py
+++ oportunidad_cita.py
@@ -2,12 +2,18 @@
 """
 Oportunidad Cita
 
-Se procesan los reportes de DGH "fecha_cita.xlsx", "oportunidad_fecha_citaBD.xlsx" y el archivo de internet "Variables.xlsx" generando como resultado:
+Este script procesa los reportes de DGH "fecha_cita.xlsx", "oportunidad_fecha_citaBD.xlsx" y el archivo de internet "Variables.xlsx" para generar los siguientes resultados:
 1. Cruce - Oportunidad_cita.xlsx
 2. Vacíos de fecha - FechaVacias.xlsx
 3. Vacíos de oportunidad - Oportunidad_citaVacias.xlsx
 4. Errores en tipo cita - Oportunidad_citaTipoCitaError.xlsx
 5. Pivotes resultante - Consulta_pivote.xlsx
+
+Pasos del proceso:
+1. Carga de archivos.
+2. Procesamiento de datos.
+3. Generación de pivotes.
+4. Descarga de archivos resultantes.
 """
 # %% Cargar archivos
 
@@ -18,13 +24,23 @@ import requests
 import duckdb
 import os
 
-print('Cargar archivos')
+print('Cargando archivos...')
 # Conectar a DuckDB y cargar los xlsx a df
 con = duckdb.connect()
 con.sql("INSTALL spatial; LOAD spatial;")
 
 # Función descargaExcel para descargar los excel compartidos en OneDrive 365
 def descargaExcel(url):
+    """
+    Descarga un archivo Excel desde una URL de OneDrive 365 y lo carga en un DataFrame.
+    
+    Args:
+    - url (str): URL del archivo Excel en OneDrive 365.
+    
+    Returns:
+    - df (DataFrame): DataFrame con los datos del archivo Excel descargado.
+    """
+
     # Reemplazar la parte después del ? con download=1
     url = url.split('?')[0] + '?download=1'
 
@@ -48,29 +64,35 @@ def descargaExcel(url):
         return False
 
 # Cargar Variables de Google Sheet
-print('-Variables')
+print('- Cargando Variables')
 dfVariables = descargaExcel("https://subredeintenorte-my.sharepoint.com/:x:/g/personal/gestiondelainformacion_subrednorte_gov_co/EXm0_LeeWK9HlP755iqoY30BW6AWaQilIXmtzlZZDMxQlQ?e=r3Pf02")
 
-print('-fecha_cita.xlsx')
+print('- Cargando fecha_cita.xlsx')
 dfFecha = con.query("SELECT * FROM st_read('fecha_cita.xlsx')").df()
 
-print('-oportunidad_fecha_citaBD.xlsx')
+print('- Cargando oportunidad_fecha_citaBD.xlsx')
 dfOportunidad = con.query("SELECT * FROM st_read('oportunidad_fecha_citaBD.xlsx')").df()
 
 # %% Procesar datos
-print('Procesar datos')
+print('Procesando datos...')
 # Agregar las vacias de dfFecha a dfOportunidad creando dfOportunidad_cita
 # Filtrar dfFecha con la columna 'Fecha de Grabacion' vacia seleccionando las columnas 'Codigo Producto (CUPS)',
 # 'Especialidad', 'Nombre Especialidad', 'Estructura Administrativa', 'Sede', 'Medico', 'Nombre Medico',
 # 'Duracion Cita', 'Servicio Nieto'
-dfFechaVacias = dfFecha[dfFecha['Fecha de Grabacion'].isna()][['Codigo Producto (CUPS)', 'Especialidad', 'Nombre Especialidad', 'Estructura Administrativa', 'Sede', 'Medico', 'Nombre Medico', 'Duracion Cita', 'Servicio Nieto']]
+dfFechaVacias = dfFecha[dfFecha['Fecha de Grabacion'].isna()][[
+    'Codigo Producto (CUPS)', 'Especialidad', 'Nombre Especialidad', 'Estructura Administrativa', 'Sede',
+    'Medico', 'Nombre Medico', 'Duracion Cita', 'Servicio Nieto']]
 
 # Crear dfOportunidad_cita uniendo dfOportunidad con dfFechaVacias
 dfOportunidad_cita = pd.concat([dfOportunidad, dfFechaVacias], ignore_index=True)
 
 # Agregar a dfOportunidad_cita desde dfFecha las columnas 'Tipo Asignacion', 'Nacionalidad', 'Barrio',
 # 'Nombre Barrio', 'Localidad', 'Nombre Localidad' cruzando con la columna 'Documento de la Cita', excluyendo las vacias, en ambas tablas
-dfOportunidad_cita = pd.merge(dfOportunidad_cita, dfFecha[['Tipo Asignacion', 'Nacionalidad', 'Barrio', 'Nombre Barrio', 'Localidad', 'Nombre Localidad', 'Documento de la Cita']].dropna(subset=['Documento de la Cita']), on='Documento de la Cita', how='left')
+dfOportunidad_cita = pd.merge(
+    dfOportunidad_cita,
+    dfFecha[['Tipo Asignacion', 'Nacionalidad', 'Barrio', 'Nombre Barrio', 'Localidad',
+    'Nombre Localidad', 'Documento de la Cita']].dropna(subset=['Documento de la Cita']),
+    on='Documento de la Cita', how='left')
 
 # Convertir la columna 'Duracion Cita' a formato datetime
 dfOportunidad_cita['Duracion Cita'] = pd.to_datetime(dfOportunidad_cita['Duracion Cita'], format='%H:%M:%S')
@@ -88,26 +110,38 @@ dfOportunidad_cita['Servicio Nieto'] = dfOportunidad_cita['Servicio Nieto'].str.
 dfVariables['Servicio Nieto'] = dfVariables['Servicio Nieto'].str.lower()
 
 # Agregar a dfOportunidad_cita desde dfVariables las columnas 'Nombre Especialidad CIP AJUST', 'nieto CIP AJUST FECHA CITA' y 'TIPO 2'
-dfOportunidad_cita = pd.merge(dfOportunidad_cita, dfVariables.drop_duplicates(subset='Servicio Nieto'), on='Servicio Nieto', how='left')
+dfOportunidad_cita = pd.merge(
+    dfOportunidad_cita,
+    dfVariables.drop_duplicates(subset='Servicio Nieto'),
+    on='Servicio Nieto', how='left')
 
 # Reportar y arreglar las colmunas con vacios
 
 # Crear dfOportunidad_citaVacias con las filas de dfOportunidad_cita que tienen campos vacios en alguna de las columnas 'Nombre Especialidad CIP AJUST', 'nieto CIP AJUST FECHA CITA' y 'TIPO 2'
-dfOportunidad_citaVacias = dfOportunidad_cita[dfOportunidad_cita['Nombre Especialidad CIP AJUST'].isna() | dfOportunidad_cita['nieto CIP AJUST FECHA CITA'].isna() | dfOportunidad_cita['TIPO 2'].isna()]
+dfOportunidad_citaVacias = dfOportunidad_cita[
+    dfOportunidad_cita['Nombre Especialidad CIP AJUST'].isna() |
+    dfOportunidad_cita['nieto CIP AJUST FECHA CITA'].isna() |
+    dfOportunidad_cita['TIPO 2'].isna()]
 
 # Reemplaza los valores vacios de 'Nombre Especialidad CIP AJUST' y 'nieto CIP AJUST FECHA CITA' por el valor de 'Servicio Nieto'
-dfOportunidad_cita['Nombre Especialidad CIP AJUST'] = dfOportunidad_cita['Nombre Especialidad CIP AJUST'].fillna(dfOportunidad_cita['Servicio Nieto'])
-dfOportunidad_cita['nieto CIP AJUST FECHA CITA'] = dfOportunidad_cita['nieto CIP AJUST FECHA CITA'].fillna(dfOportunidad_cita['Servicio Nieto'])
+dfOportunidad_cita['Nombre Especialidad CIP AJUST'] = dfOportunidad_cita[
+    'Nombre Especialidad CIP AJUST'].fillna(dfOportunidad_cita['Servicio Nieto'])
+dfOportunidad_cita['nieto CIP AJUST FECHA CITA'] = dfOportunidad_cita[
+    'nieto CIP AJUST FECHA CITA'].fillna(dfOportunidad_cita['Servicio Nieto'])
 
 # Reportar 'TIPO CITA' con errores
 
 # Filtrar 'TIPO CITA' si tiene valor 'Remision' o si es vacío y 'Identificacion' no es vacío
-dfOportunidad_citaTipoCitaError = dfOportunidad_cita[(dfOportunidad_cita['TIPO CITA'] == 'Remision') | (dfOportunidad_cita['TIPO CITA'].isna() & dfOportunidad_cita['Identificacion'].notnull())]
+dfOportunidad_citaTipoCitaError = dfOportunidad_cita[
+    (dfOportunidad_cita['TIPO CITA'] == 'Remision') |
+    (dfOportunidad_cita['TIPO CITA'].isna() &
+    dfOportunidad_cita['Identificacion'].notnull())]
 
 # Ordenar
 
 # Ordenar por 'Sede' (Nombre compañia), 'Nombre Especialidad', 'Nombre Medico', 'Fecha Cita', 'Hora Cita', 'Documento de la Cita
-dfOportunidad_cita = dfOportunidad_cita.sort_values(by=[ 'Sede', 'Nombre Especialidad', 'Nombre Medico', 'Fecha Cita', 'Hora Cita', 'Documento de la Cita'])
+dfOportunidad_cita = dfOportunidad_cita.sort_values(
+    by=['Sede', 'Nombre Especialidad', 'Nombre Medico', 'Fecha Cita', 'Hora Cita', 'Documento de la Cita'])
 
 # Oportunidad
 
@@ -153,42 +187,50 @@ dfOportunidad_cita['Reprogramadas'] = np.where(
 dfOportunidad_cita_Atendida = dfOportunidad_cita[dfOportunidad_cita['Estado Cita'] == 'Atendida']
 
 # Crear la tabla pivote Asignadas, crea columna 'Asignadas'
-pivote_Asignadas = dfOportunidad_cita.groupby(['TIPO 2', 'Sede', 'nieto CIP AJUST FECHA CITA']).size().reset_index(name='Asignadas')
+pivote_Asignadas = dfOportunidad_cita.groupby(
+    ['TIPO 2', 'Sede', 'nieto CIP AJUST FECHA CITA']).size().reset_index(name='Asignadas')
 
 # Pivote Estado
 
 # Crear la tabla pivote Estados, crea columnas 'Atendida_x', 'Cancelada_x', 'Incumplida_x'
-pivote_Estados = dfOportunidad_cita.pivot_table(index=['TIPO 2', 'Sede', 'nieto CIP AJUST FECHA CITA'], columns='Estado Cita', aggfunc='size', fill_value=0).reset_index()
+pivote_Estados = dfOportunidad_cita.pivot_table(
+    index=['TIPO 2', 'Sede', 'nieto CIP AJUST FECHA CITA'], columns='Estado Cita', aggfunc='size', fill_value=0).reset_index()
 
 # Pivote Reprogramadas
 
 # Crear la tabla pivote Estados, crea columnas 'Atendida_y', 'Cancelada_y', 'Incumplida_y', 'No Reprogramada', 'Reprogramada'
-pivote_Reprogramadas = dfOportunidad_cita.pivot_table(index=['TIPO 2', 'Sede', 'nieto CIP AJUST FECHA CITA'], columns='Reprogramadas', aggfunc='size', fill_value=0).reset_index()
+pivote_Reprogramadas = dfOportunidad_cita.pivot_table(
+    index=['TIPO 2', 'Sede', 'nieto CIP AJUST FECHA CITA'], columns='Reprogramadas', aggfunc='size', fill_value=0).reset_index()
 
 # Pivote Atendida Tipo
 
 # Crear la tabla pivote Atendida Tipo, crea columnas 'Control_x', 'Primer Vez_x', 'Remision_x'
-pivote_AtendidaTipo = dfOportunidad_cita_Atendida.pivot_table(index=['TIPO 2', 'Sede', 'nieto CIP AJUST FECHA CITA'], columns='TIPO CITA', aggfunc='size', fill_value=0).reset_index()
+pivote_AtendidaTipo = dfOportunidad_cita_Atendida.pivot_table(
+    index=['TIPO 2', 'Sede', 'nieto CIP AJUST FECHA CITA'], columns='TIPO CITA', aggfunc='size', fill_value=0).reset_index()
 
 # Pivote Atendida Vinculacion
 
 # Crear la tabla pivote Atendida Vinculacion, crea columnas 'Contributivo', 'Otro', 'Subsidiado', 'Vinculado'
-pivote_AtendidaVinculacion = dfOportunidad_cita_Atendida.pivot_table(index=['TIPO 2', 'Sede', 'nieto CIP AJUST FECHA CITA'], columns='Vinculacion', aggfunc='size', fill_value=0).reset_index()
+pivote_AtendidaVinculacion = dfOportunidad_cita_Atendida.pivot_table(
+    index=['TIPO 2', 'Sede', 'nieto CIP AJUST FECHA CITA'], columns='Vinculacion', aggfunc='size', fill_value=0).reset_index()
 
 # Pivote Tipo
 
 # Crear la tabla pivote Tipo, crea columnas 'Control_y', 'Primer Vez_y', 'Remision_y'
-pivote_Tipo = dfOportunidad_cita.pivot_table(index=['TIPO 2', 'Sede', 'nieto CIP AJUST FECHA CITA'], columns='TIPO CITA', aggfunc='size', fill_value=0).reset_index()
+pivote_Tipo = dfOportunidad_cita.pivot_table(
+    index=['TIPO 2', 'Sede', 'nieto CIP AJUST FECHA CITA'], columns='TIPO CITA', aggfunc='size', fill_value=0).reset_index()
 
 # Pivote Tipo Oportunidad
 
 # Crear la tabla pivote Tipo Oportunidad, crea columnas 'Control', 'Primer Vez', 'Remision'
-pivote_TipoOportunidad = dfOportunidad_cita.pivot_table(values='Oportunidad', index=['TIPO 2', 'Sede', 'nieto CIP AJUST FECHA CITA'], columns='TIPO CITA', aggfunc='sum', fill_value=0).reset_index()
+pivote_TipoOportunidad = dfOportunidad_cita.pivot_table(
+    values='Oportunidad', index=['TIPO 2', 'Sede', 'nieto CIP AJUST FECHA CITA'], columns='TIPO CITA', aggfunc='sum', fill_value=0).reset_index()
 
 # Pivote Tipo Asignacion
 
 # Crear la tabla pivote Tipo Asignacion, crea columnas 'Extra', 'Normal'
-pivote_TipoAsignacion = dfOportunidad_cita_Atendida.pivot_table(index=['TIPO 2', 'Sede', 'nieto CIP AJUST FECHA CITA'], columns='Tipo Asignacion', aggfunc='size', fill_value=0).reset_index()
+pivote_TipoAsignacion = dfOportunidad_cita_Atendida.pivot_table(
+    index=['TIPO 2', 'Sede', 'nieto CIP AJUST FECHA CITA'], columns='Tipo Asignacion', aggfunc='size', fill_value=0).reset_index()
 
 # Combinar pivotes en dfConsulta_externa
 
@@ -208,36 +250,41 @@ dfConsulta_pivote['Reales'] = dfConsulta_pivote['Asignadas'] + dfConsulta_pivote
 dfConsulta_pivote['horas_inf'] = dfConsulta_pivote['Asignadas'] / 3
 
 # Crear 'oportunidad_primera_vez' dividiendo 'Primer Vez_y' por 'Primer Vez' y manejando errores con np.where
-dfConsulta_pivote['oportunidad_primera_vez'] = np.where(dfConsulta_pivote['Primer Vez_y'] != 0, dfConsulta_pivote['Primer Vez'] / dfConsulta_pivote['Primer Vez_y'], 0)
+dfConsulta_pivote['oportunidad_primera_vez'] = np.where(
+    dfConsulta_pivote['Primer Vez_y'] != 0, dfConsulta_pivote['Primer Vez'] / dfConsulta_pivote['Primer Vez_y'], 0)
 
 # Crear 'oportunidad_control' dividiendo 'Primer Vez_y' por 'Primer Vez' y manejando errores con np.where
 #dfConsulta_pivote['oportunidad_control'] = np.where(dfConsulta_pivote['Control_y'] != 0, dfConsulta_pivote['Control'] / dfConsulta_pivote['Control_y'], 0)
 
 # Crear 'val1' con valor 'Ok' si 'Primer Vez_y' + 'Control_y' = 'Asignadas', de lo contrario 'Error'
-dfConsulta_pivote['val1'] = np.where(dfConsulta_pivote['Primer Vez_y'] + dfConsulta_pivote['Control_y'] ==
-                                      dfConsulta_pivote['Asignadas'], 'Ok', 'Error')
+dfConsulta_pivote['val1'] = np.where(
+    dfConsulta_pivote['Primer Vez_y'] + dfConsulta_pivote['Control_y'] ==
+    dfConsulta_pivote['Asignadas'], 'Ok', 'Error')
 
 # Crear 'val2' con valor 'Ok' si ('Vinculado' + 'Subsidiado' + 'Contributivo' + 'Otro') = ('Primer Vez_x' + 'Control_x'), de lo contrario 'Error'
-dfConsulta_pivote['val2'] = np.where(dfConsulta_pivote['Vinculado'] + dfConsulta_pivote['Subsidiado'] + dfConsulta_pivote['Contributivo'] + dfConsulta_pivote['Otro'] ==
-                                     dfConsulta_pivote['Primer Vez_x'] + dfConsulta_pivote['Control_x'], 'Ok', 'Error')
+dfConsulta_pivote['val2'] = np.where(
+    dfConsulta_pivote['Vinculado'] + dfConsulta_pivote['Subsidiado'] + dfConsulta_pivote['Contributivo'] + dfConsulta_pivote['Otro'] ==
+    dfConsulta_pivote['Primer Vez_x'] + dfConsulta_pivote['Control_x'], 'Ok', 'Error')
 
 # Seleccionar las columnas finales
-dfConsulta_pivote = dfConsulta_pivote[['TIPO 2', 'Sede', 'nieto CIP AJUST FECHA CITA', 'Asignadas', 'Reales',
-                                       'Asignadas', 'Reprogramada', 'Cancelada_x', 'Incumplida_x', 'Primer Vez_x',
-                                       'Control_x', 'Vinculado', 'Subsidiado', 'Contributivo', 'Otro',
-                                       'horas_inf', 'Primer Vez_y', 'Primer Vez', 'oportunidad_primera_vez', 'Control_y',
-                                       'Control','val1', 'val2']]
+dfConsulta_pivote = dfConsulta_pivote[[
+    'TIPO 2', 'Sede', 'nieto CIP AJUST FECHA CITA', 'Asignadas', 'Reales',
+    'Asignadas', 'Reprogramada', 'Cancelada_x', 'Incumplida_x', 'Primer Vez_x',
+    'Control_x', 'Vinculado', 'Subsidiado', 'Contributivo', 'Otro',
+    'horas_inf', 'Primer Vez_y', 'Primer Vez', 'oportunidad_primera_vez', 'Control_y',
+    'Control','val1', 'val2']]
 
 # Cambiar los nombres de las columnas
-dfConsulta_pivote.columns = ['Tipo', 'Sede', 'Nieto', 'Programado', 'Reales',
-                             'Asignadas','Reprogramada', 'Cancelada', 'Inasistencia', 'Primer_Vez',
-                             'Control','Vinculado', 'Subsidiado', 'Contributivo', 'Otro',
-                             'horas_inf', 'asig_primer_vez', 'dias_primer_vez', 'oportunidad_primera_vez', 'asig_control',
-                             'dias_control','val1', 'val2']
+dfConsulta_pivote.columns = [
+    'Tipo', 'Sede', 'Nieto', 'Programado', 'Reales',
+    'Asignadas','Reprogramada', 'Cancelada', 'Inasistencia', 'Primer_Vez',
+    'Control','Vinculado', 'Subsidiado', 'Contributivo', 'Otro',
+    'horas_inf', 'asig_primer_vez', 'dias_primer_vez', 'oportunidad_primera_vez', 'asig_control',
+    'dias_control','val1', 'val2']
 
 # %% Descargar los archivos
 
-print('Descargar archivos')
+print('Descargando archivos ...')
 # Convertir las columnas tipo fecha/hora a solo fecha en texto
 dfOportunidad_cita['Fecha de Grabacion'] = dfOportunidad_cita['Fecha de Grabacion'].dt.strftime('%d/%m/%Y')
 dfOportunidad_cita['Fecha Cita'] = dfOportunidad_cita['Fecha Cita'].dt.strftime('%d/%m/%Y')
@@ -245,13 +292,13 @@ dfOportunidad_cita['Fecha Cita'] = dfOportunidad_cita['Fecha Cita'].dt.strftime(
 dfOportunidad_cita['Fecha Nacimiento'] = dfOportunidad_cita['Fecha Nacimiento'].dt.strftime('%d/%m/%Y')
 
 # Convertir los df a xlsx
-print('-Oportunidad_cita.xlsx')
+print('- Descargando Oportunidad_cita.xlsx')
 con.execute("COPY (SELECT * FROM dfOportunidad_cita) TO 'Oportunidad_cita.xlsx' WITH (FORMAT GDAL, DRIVER 'xlsx');")
-print('-FechaVacias.xlsx')
+print('- Descargando FechaVacias.xlsx')
 con.execute("COPY (SELECT * FROM dfFechaVacias) TO 'FechaVacias.xlsx' WITH (FORMAT GDAL, DRIVER 'xlsx');")
-print('-Oportunidad_citaVacias.xlsx')
+print('- Descargando Oportunidad_citaVacias.xlsx')
 con.execute("COPY (SELECT * FROM dfOportunidad_citaVacias) TO 'Oportunidad_citaVacias.xlsx' WITH (FORMAT GDAL, DRIVER 'xlsx');")
-print('-Oportunidad_citaTipoCitaError.xlsx')
+print('- Descargando Oportunidad_citaTipoCitaError.xlsx')
 con.execute("COPY (SELECT * FROM dfOportunidad_citaTipoCitaError) TO 'Oportunidad_citaTipoCitaError.xlsx' WITH (FORMAT GDAL, DRIVER 'xlsx');")
-print('-Consulta_pivote.xlsx')
+print('- Descargando Consulta_pivote.xlsx')
 con.execute("COPY (SELECT * FROM dfConsulta_pivote) TO 'Consulta_pivote.xlsx' WITH (FORMAT GDAL, DRIVER 'xlsx');")
